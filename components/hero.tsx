2c9080a5f35f180f46d3086f601e07b9cf367b43
Hossam Mansour <105172548+0xHos@users.noreply.github.com>
2026-02-15 00:24:09 -0500

    image

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
-          src="/images/hero.jpg"
+          src="https://plus.unsplash.com/premium_photo-1661962841993-99a07c27c9f4?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Luxury interior design"
           fill
           className="object-cover"
